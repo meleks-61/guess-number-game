c9e37ebc7125a491cc4290bdaa76ffcd3cd438e9
Melek <maibalonn@gmail.com>
2021-08-07 12:56:25 +0300

    begining

---
+++ app.js
@@ -0,0 +1,15 @@
+const inputElement =document.querySelector("#number");
+// console.log(inputElement.value);
+const checkElement=document.querySelector("#check");
+
+
+
+
+
+checkElement.addEventListener("click", ()=>{
+    console.log(inputElement.value)
+    
+    
+});
+
+// console.log(document.getElementById("number").value);
