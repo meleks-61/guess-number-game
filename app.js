449a78b6d12a797658d439ec726031862de6cb17
Melek <maibalonn@gmail.com>
2021-08-14 00:06:41 +0300

    improving commit

--- app.js
+++ app.js
@@ -5,9 +5,93 @@ const inputElement =document.querySelector("#number");
 
 const checkElement=document.querySelector("#check");
 
+const answerElement=document.querySelector("#answer")
+
+const gifElement=document.querySelector("#gif")
+
 const randomNumber = ()=>{
     return Math.floor(Math.random()*100);
+
 }
+const a =randomNumber();
+
+
+
+
+
+
+//Functions
+
+
+function checkF(){
+    
+    const b =parseFloat(inputElement.value);
+    
+
+
+    
+
+    if ( b === a) {
+
+        
+        gifElement.src="./img/tenor.gif";
+        answerElement.innerHTML =""
+
+        console.log("tebrikler");
+        // answerElement.classList.add('animate__animated', 'animate__flash');
+        answerElement.style.backgroundColor="white"
+        count+=1
+  
+    }  
+    else if ((100 >= b) && (b > a)){
+
+        answerElement.innerHTML="<h3 >please enter smaler number</h3>";
+        console.log("please enter smaler number");
+        answerElement.classList.add('animate__animated', 'animate__wobble');
+
+        answerElement.style.backgroundColor = "lightblue";
+        answerElement.style.borderRadius="50%";
+        
+
+       
+    
+        
+    }else if ((0<= b) && (b < a)) {
+        answerElement.innerHTML="<h3>please enter bigger number</h3>";
+        answerElement.classList.add('animate__animated', 'animate__tada');
+        answerElement.style.backgroundColor = "yellow";
+        answerElement.style.borderRadius="50%"
+
+        console.log("please enter bigger number");
+        
+       
+    
+    }else if (b>100 || b< 0 ) {
+
+        answerElement.innerHTML="<h3>please enter number in 0-100</h3>"
+        answerElement.classList.add('animate__animated', 'animate__bounce');
+
+        console.log("please enter nuber in 0-100");
+        answerElement.style.backgroundColor = "oranged";
+        answerElement.style.borderRadius="50%";
+        
+    }else if(isNaN(b)){
+        answerElement.innerHTML="<h3>invalid syntax</h3>";
+        answerElement.classList.add('animate__animated', 'animate__bounce');
+        answerElement.style.backgroundColor = "oranged";
+        answerElement.style.borderRadius="50%";
+    }
+        
+    console.log(a);
+    console.log(b);
+    console.log();
+    // console.log(inputElement.value);
+    // console.log(typeof(inputElement.value));    
+    // console.log(typeof(answerElement))
+    
+};
+
+
 
 
 
@@ -24,32 +108,4 @@ checkElement.addEventListener("click", ()=>{
 
 
 //Functions
-
-function checkF(){
-    const a =randomNumber()
-    const b =parseFloat(inputElement.value)
-
-    if ( b === a) {
-
-            console.log("tebrikler");
-  
-    }  
-    else if (b > a) {
-        console.log("please enter smaler number");
-    
-        
-    }else if (b < a) {
-        console.log("please enter bigger number");
-    
-    }if (inputElement.value>100 || inputElement.value< 0 ) {
-        console.log("please enter nuber in 0-100");
-        
-    }
-        
-    console.log(a);
-    console.log(b);
-    // console.log(inputElement.value);
-    // console.log(typeof(inputElement.value));    
-    
-};
 
